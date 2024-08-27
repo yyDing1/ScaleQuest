93e2c8b3483da2c5fec9e51e62750906b3ad7138
kyle-Shi <kylesxy@gmail.com>
2024-08-27 15:58:26 +0800

    update evaluation scripts

---
+++ evaluation/scripts/math_eval.sh
@@ -0,0 +1,134 @@
+set -ex
+
+export CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7"
+
+PROMPT_TYPE=deepseek-math
+MODEL_NAME_OR_PATH=/data/dyy/dart-math/outputs/dart-math-deepseek-7b-Qwen2-MathGen-140K/checkpoint-1250
+OUTPUT_DIR=outputs
+
+SPLIT="test"
+NUM_TEST_SAMPLE=-1
+
+# English open datasets
+DATA_NAME="gsm8k,math"
+TOKENIZERS_PARALLELISM=false \
+python3 -u math/math_eval.py \
+    --model_name_or_path ${MODEL_NAME_OR_PATH} \
+    --data_name ${DATA_NAME} \
+    --output_dir ${OUTPUT_DIR} \
+    --split ${SPLIT} \
+    --prompt_type ${PROMPT_TYPE} \
+    --num_test_sample ${NUM_TEST_SAMPLE} \
+    --seed 0 \
+    --temperature 0 \
+    --n_sampling 1 \
+    --top_p 1 \
+    --start 0 \
+    --end -1 \
+    --use_vllm \
+    --save_outputs \
+    --overwrite \
+
+# # English open datasets
+# DATA_NAME="gsm8k,math,svamp,asdiv,mawps,carp_en,tabmwp,minerva_math,gaokao2023en,olympiadbench,college_math"
+# TOKENIZERS_PARALLELISM=false \
+# python3 -u math_eval.py \
+#     --model_name_or_path ${MODEL_NAME_OR_PATH} \
+#     --data_name ${DATA_NAME} \
+#     --output_dir ${OUTPUT_DIR} \
+#     --split ${SPLIT} \
+#     --prompt_type ${PROMPT_TYPE} \
+#     --num_test_sample ${NUM_TEST_SAMPLE} \
+#     --seed 0 \
+#     --temperature 0 \
+#     --n_sampling 1 \
+#     --top_p 1 \
+#     --start 0 \
+#     --end -1 \
+#     --use_vllm \
+#     --save_outputs \
+#     --overwrite \
+
+# # English multiple-choice datasets
+# DATA_NAME="aqua,sat_math,mmlu_stem"
+# TOKENIZERS_PARALLELISM=false \
+# python3 -u math_eval.py \
+#     --model_name_or_path ${MODEL_NAME_OR_PATH} \
+#     --data_name ${DATA_NAME} \
+#     --output_dir ${OUTPUT_DIR} \
+#     --split ${SPLIT} \
+#     --prompt_type ${PROMPT_TYPE} \
+#     --num_test_sample ${NUM_TEST_SAMPLE} \
+#     --seed 0 \
+#     --temperature 0 \
+#     --n_sampling 1 \
+#     --top_p 1 \
+#     --start 0 \
+#     --end -1 \
+#     --use_vllm \
+#     --save_outputs \
+#     --overwrite \
+#     --num_shots 5
+
+# # Chinese gaokao collections
+# DATA_NAME="gaokao2024_I,gaokao2024_II,gaokao2024_mix,gaokao_math_cloze,gaokao_math_qa"
+# TOKENIZERS_PARALLELISM=false \
+# python3 -u math_eval.py \
+#     --model_name_or_path ${MODEL_NAME_OR_PATH} \
+#     --data_name ${DATA_NAME} \
+#     --output_dir ${OUTPUT_DIR} \
+#     --split ${SPLIT} \
+#     --prompt_type ${PROMPT_TYPE} \
+#     --num_test_sample ${NUM_TEST_SAMPLE} \
+#     --seed 0 \
+#     --temperature 0 \
+#     --n_sampling 1 \
+#     --top_p 1 \
+#     --start 0 \
+#     --end -1 \
+#     --use_vllm \
+#     --save_outputs \
+#     --overwrite \
+#     --adapt_few_shot
+
+# # Chinese other datasets
+# DATA_NAME="cmath,cn_middle_school"
+# TOKENIZERS_PARALLELISM=false \
+# python3 -u math_eval.py \
+#     --model_name_or_path ${MODEL_NAME_OR_PATH} \
+#     --data_name ${DATA_NAME} \
+#     --output_dir ${OUTPUT_DIR} \
+#     --split ${SPLIT} \
+#     --prompt_type ${PROMPT_TYPE} \
+#     --num_test_sample ${NUM_TEST_SAMPLE} \
+#     --seed 0 \
+#     --temperature 0 \
+#     --n_sampling 1 \
+#     --top_p 1 \
+#     --start 0 \
+#     --end -1 \
+#     --use_vllm \
+#     --save_outputs \
+#     --overwrite \
+#     --adapt_few_shot
+
+
+# # English competition datasets
+# DATA_NAME="aime24,amc23"
+# TOKENIZERS_PARALLELISM=false \
+# python3 -u math_eval.py \
+#     --model_name_or_path ${MODEL_NAME_OR_PATH} \
+#     --data_name ${DATA_NAME} \
+#     --output_dir ${OUTPUT_DIR} \
+#     --split ${SPLIT} \
+#     --prompt_type ${PROMPT_TYPE} \
+#     --num_test_sample ${NUM_TEST_SAMPLE} \
+#     --seed 0 \
+#     --temperature 0 \
+#     --n_sampling 1 \
+#     --top_p 1 \
+#     --start 0 \
+#     --end -1 \
+#     --use_vllm \
+#     --save_outputs \
+#     --overwrite \
